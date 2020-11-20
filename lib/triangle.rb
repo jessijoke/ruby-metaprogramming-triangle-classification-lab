8db99827b29a630bf14cd7197c8f54d7220f1642
jessijoke <jessijoke@yahoo.com>
2020-11-19 20:38:43 -0500

    commit tree 6bc1c0bb817e6da8c40028ec848d58df67ab5b17

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
+  attr_reader :equilateral, :isosceles, :scalene
   # write code here
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if @x <= 0 || @y <= 0 || @z <= 0 || !@x || !@y || !@z || (@x + @y) <= @z || (@y + @z) <= @x || (@z + @x) <= @y
+      raise TriangleError
+    end
+      return :equilateral if @x == @y && @x == @z && @y == @z
+      return :isosceles if @x == @y || @x == @z || @y == @z
+      return :scalene if @x != @y && @x != @z && @y != @z
+      
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
